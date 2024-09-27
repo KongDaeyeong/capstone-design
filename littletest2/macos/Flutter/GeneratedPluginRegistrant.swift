88bba8e5b2938f05c362aebe79e8b35df5e039dd
KongDaeyoeng <kong029742@gmail.com>
2024-09-27 11:59:11 +0900

    기기 상태, 로그, 알림 기능 병합

--- littletest2/macos/Flutter/GeneratedPluginRegistrant.swift
+++ littletest2/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_blue_plus
+import flutter_local_notifications
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
 }
