d99f06015af27d2ef9452ebc1aa1b916f54ec67c
KongDaeyoeng <kong029742@gmail.com>
2024-10-10 15:23:46 +0900

    AWS Cognito 기반 로그인 기능 추가

--- littletest2/macos/Flutter/GeneratedPluginRegistrant.swift
+++ littletest2/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import flutter_blue_plus
 import flutter_local_notifications
+import flutter_secure_storage_macos
 import path_provider_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
